066b5fd40331a0b15ca75e9642d1b8ae848143a7
billusleshi <129374969+billusleshi@users.noreply.github.com>
2024-05-09 17:17:19 +0300

    commited

--- backend/models/cities.py
+++ backend/models/cities.py
@@ -1,30 +1,36 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date, Time, Float
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
+# Define the Base class for declarative class definitions
 Base = declarative_base()
 
+# Define class for cities_chart_data table
 class CitiesChartData(Base):
     __tablename__ = 'cities_chart_data'
 
-    Date = Column(Date, primary_key=True)
-    Cities = Column(String, primary_key=True)
-    CityName = Column(String)
-    Views = Column(Integer)
+    # Define columns
+    date = Column(Date, primary_key=True)
+    cities = Column(String, primary_key=True)
+    city_name = Column(String)
+    views = Column(Integer)
 
+# Define class for cities_table_data table
 class CitiesTableData(Base):
     __tablename__ = 'cities_table_data'
 
-    Cities = Column(String, primary_key=True)
-    CityName = Column(String, primary_key=True)
-    Geography1 = Column(String)
-    Geography2 = Column(String)
-    Views = Column(Integer)
-    WatchTime = Column(Float)
-    AverageViewDuration = Column(Time)
+    # Define columns
+    cities = Column(String, primary_key=True)
+    city_name = Column(String, primary_key=True)
+    geography1 = Column(String)
+    geography2 = Column(String)
+    views = Column(Integer)
+    watch_time = Column(Float)
+    average_view_duration = Column(Time)
 
+# Define class for cities_totals table
 class CitiesTotals(Base):
     __tablename__ = 'cities_totals'
 
-    Date = Column(Date, primary_key=True)
-    Views = Column(Integer)
+    # Define columns
+    date = Column(Date, primary_key=True)
+    views = Column(Integer)
